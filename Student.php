b116b19611ebae02e772ca4a0516ee20be88b6b7
Paul Li <paulli@shaw.ca>
2018-01-10 09:53:23 -0800

    Added Student class with basic functions

---
+++ Student.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of Student
+ *
+ * @author Paul
+ */
+class Student {
+    
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    
+    function add_email($which, $address) {
+        $this->emails[$which] = $address;
+    }
+    
+    function add_grade($grade)
+    {
+        $this->grades[] = $grade;
+    }
+    
+}
